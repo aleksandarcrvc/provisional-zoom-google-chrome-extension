cd3e323c899eb49355b3bc772358e9f86e9eb2be
aleksandarcrvc <aleksandarcrvc@gmail.com>
2016-10-09 14:38:35 +0200

    setup jasmine test runner with gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,16 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+
+const jasmine = require('gulp-jasmine-livereload-task');
+
+gulp.task('test', jasmine({
+    files: [
+        './src/**/*.js', 
+        '!./src/vendor/**/*.js', 
+        './specs/**/*.js'
+        ],
+    port: 8080,
+}));
  
 gulp.task('pack-extension', () => {
     return gulp.src('src/*')
